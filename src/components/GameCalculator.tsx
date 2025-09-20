05f1ef20ca913332fc53f888cf9cf00add6aea6d
SakaKishiyami <tiki.tabernik@gmail.com>
2025-09-19 23:54:32 -0400

    Update inventory: Remove WIP skill elixirs and add new skill elixir random items
    - Removed Skill4WIP, Skill5WIP, Skill6WIP, Skill8WIP from asset lists
    - Added SkillElixirRandom5, SkillElixirRandom6, SkillElixirRandom7, SkillElixirRandom8
    - Reordered skill elixir random items to: 8,7,6,5,4,3,2,1, then normal random
    - Updated both asset loading list and WardenItems category list
    - Renamed image files to match new naming convention

--- src/components/GameCalculator.tsx
+++ src/components/GameCalculator.tsx
@@ -830,9 +830,9 @@ export default function GameCalculator() {
       'NewSummonCoin', 'NewSummonCoinPart', 'NightfallEquip', 'NightfallMedal', 'NightfallRing', 'NightfallSuit',
       'NoviceLeague1', 'Plazma', 'PocketWatch', 'PremiumGiftBox3', 'PressCard', 'Prestige1', 'Prestige2', 'Purple',
       'RandomRingBox', 'RandomScriptPart', 'RandomScroll', 'RematchFlag', 'RenameCard', 'ResourceCollectorCard',
-      'RingOfChange', 'RoseBouquet1', 'RubyRing2', 'SanctuaryStandardFlag', 'Skill4WIP', 'Skill500', 'Skill5WIP',
-      'Skill6WIP', 'Skill8WIP', 'SkillElixir1', 'SkillElixir2', 'SkillElixir3', 'SkillElixir4', 'SkillElixirRandom100',
-      'SkillElixirRandom1000', 'SkillElixirRandom50', 'SolidarityStandardFlag', 'SophisticatedSatin', 'Spirit1',
+      'RingOfChange', 'RoseBouquet1', 'RubyRing2', 'SanctuaryStandardFlag',       'Skill500', 'SkillElixir1', 'SkillElixir2', 'SkillElixir3', 'SkillElixir4', 'SkillElixirRandom8', 'SkillElixirRandom7',
+      'SkillElixirRandom6', 'SkillElixirRandom5', 'SkillElixirRandom4', 'SkillElixirRandom3', 'SkillElixirRandom2', 'SkillElixirRandom1',
+      'SkillElixirRandom', 'SkillElixirRandom100', 'SkillElixirRandom1000', 'SkillElixirRandom50', 'SolidarityStandardFlag', 'SophisticatedSatin', 'Spirit1',
       'Spirit15(1)', 'Spirit15(2)', 'Spirit2', 'Spirit3', 'Spirit4', 'Spirit5', 'SpiritScript', 'Strength1',
       'Strength15(1)', 'Strength15(2)', 'Strength2', 'Strength3', 'Strength4', 'Strength6', 'StrengthScript',
       'SupremacyBadge', 'SupremacyBadgePart', 'SupremacyBadgeRandomBox', 'Talent1', 'Talent2', 'Talent3', 'Talent4',
@@ -1438,7 +1438,7 @@ export default function GameCalculator() {
   const getItemsByCategory = (category: string): string[] => {
     const itemLists = {
       'Resources': ['Blood5M', 'BloodRandom', 'Blood100K', 'Blood7', 'Blood6', 'Blood5', 'Blood4', 'Blood3', 'Blood2', 'Blood1', 'Nectar5M', 'NectarRandom', 'Nectar100K', 'Nectar7', 'Nectar6', 'Nectar5', 'Nectar4', 'Nectar3', 'Nectar2', 'Nectar1', 'Bats5M', 'BatRandom', 'Bat100K', 'Bat7', 'Bat6', 'Bat5', 'Bat4', 'Bat3', 'Bat2', 'Bat1', 'ResourceCollectorCard'],
-      'WardenItems': ['BloodOriginToken', 'SupremacyBadge', 'SupremacyBadgePart', 'MacabrianCoin', 'MacabrianCoinPart', 'CircusTicket', 'CircusTicketPart', 'CityBadge', 'CityBadgePart', 'ArenaTrophy', 'ArenaTrophyPart', 'NewSummonCoin', 'NewSummonCoinPart', 'SupremacyBadgeRandomBox', 'MacabrianCoinRandomBox', 'CircusTicketRandomBox', 'CityBadgeRandomBox', 'RandomScroll', 'RandomScriptPart', 'StrengthScript', 'AllureScript', 'IntellectScript', 'SpiritScript', 'TalentScroll6Star', 'TalentScroll5Star', 'TalentScroll4Star', 'TalentScroll3Star', 'TalentScroll2Star', 'TalentScroll1Star', 'TalentRandom5', 'TalentScroll7', 'TalentScroll6', 'TalentScroll5', 'TalentScroll4', 'TalentScroll3', 'TalentScroll2', 'TalentScroll1', 'TalentScroll50', 'TalentScroll100', 'TalentScroll200', 'Talent4', 'Talent3', 'Talent2', 'Talent1', 'Mystery1', 'Mystery2', 'Mystery3', 'Mystery4', 'Mystery5', 'Mystery15(1)', 'Mystery15(2)', 'Strength1', 'Strength2', 'Strength3', 'Strength4', 'Strength6', 'Strength15(1)', 'Strength15(2)', 'Allure1', 'Allure2', 'Allure3', 'Allure4', 'Allure5', 'Allure15(1)', 'Allure15(2)', 'Intellect1', 'Intellect2', 'Intellect3', 'Intellect4', 'Intellect5', 'Intellect15(1)', 'Intellect15(2)', 'Spirit1', 'Spirit2', 'Spirit3', 'Spirit4', 'Spirit5', 'Spirit15(1)', 'Spirit15(2)', 'Dominance4', 'Dominance3', 'Dominance2', 'Dominance1', 'DominanceBox', 'Skill8', 'Skill6', 'Skill4', 'Skill5', 'Skill500', 'SkillElixirRandom1000', 'SkillElixirRandom100', 'SkillElixirRandom50', 'SkillElixir4', 'SkillElixir3', 'SkillElixir2', 'SkillElixir1'],
+      'WardenItems': ['BloodOriginToken', 'SupremacyBadge', 'SupremacyBadgePart', 'MacabrianCoin', 'MacabrianCoinPart', 'CircusTicket', 'CircusTicketPart', 'CityBadge', 'CityBadgePart', 'ArenaTrophy', 'ArenaTrophyPart', 'NewSummonCoin', 'NewSummonCoinPart', 'SupremacyBadgeRandomBox', 'MacabrianCoinRandomBox', 'CircusTicketRandomBox', 'CityBadgeRandomBox', 'RandomScroll', 'RandomScriptPart', 'StrengthScript', 'AllureScript', 'IntellectScript', 'SpiritScript', 'TalentScroll6Star', 'TalentScroll5Star', 'TalentScroll4Star', 'TalentScroll3Star', 'TalentScroll2Star', 'TalentScroll1Star', 'TalentRandom5', 'TalentScroll7', 'TalentScroll6', 'TalentScroll5', 'TalentScroll4', 'TalentScroll3', 'TalentScroll2', 'TalentScroll1', 'TalentScroll50', 'TalentScroll100', 'TalentScroll200', 'Talent4', 'Talent3', 'Talent2', 'Talent1', 'Mystery1', 'Mystery2', 'Mystery3', 'Mystery4', 'Mystery5', 'Mystery15(1)', 'Mystery15(2)', 'Strength1', 'Strength2', 'Strength3', 'Strength4', 'Strength6', 'Strength15(1)', 'Strength15(2)', 'Allure1', 'Allure2', 'Allure3', 'Allure4', 'Allure5', 'Allure15(1)', 'Allure15(2)', 'Intellect1', 'Intellect2', 'Intellect3', 'Intellect4', 'Intellect5', 'Intellect15(1)', 'Intellect15(2)', 'Spirit1', 'Spirit2', 'Spirit3', 'Spirit4', 'Spirit5', 'Spirit15(1)', 'Spirit15(2)', 'Dominance4', 'Dominance3', 'Dominance2', 'Dominance1', 'DominanceBox', 'Skill8', 'Skill6', 'Skill4', 'Skill5', 'Skill500', 'SkillElixirRandom8', 'SkillElixirRandom7', 'SkillElixirRandom6', 'SkillElixirRandom5', 'SkillElixirRandom4', 'SkillElixirRandom3', 'SkillElixirRandom2', 'SkillElixirRandom1', 'SkillElixirRandom', 'SkillElixirRandom1000', 'SkillElixirRandom100', 'SkillElixirRandom50', 'SkillElixir4', 'SkillElixir3', 'SkillElixir2', 'SkillElixir1'],
       'Lover+ChildItems': ['AgneyiToken', 'CulannToken', 'HelaToken', 'ArtLoverToken', 'DailySecretPack', 'IntimacyCase', 'IntimacyBag', 'IntimacyPurse', 'PremiumGiftBox3', 'DeluxeGiftBox2', 'GiftBox1', 'Intimacy4', 'Intimacy3', 'Intimacy2', 'Intimacy1', 'TourMap', 'CrateOfDrinks', 'Beast', 'Vito', 'Plazma', 'DailyAttractionBox', 'CharmBox', 'CharmBottle2', 'CharmPhial1', 'IndigoBouquet3', 'AzureBouquet2', 'RoseBouquet1', 'Attraction4', 'Attraction3', 'Attraction2', 'Attraction1', 'BlackPearlRing3', 'RubyRing2', 'AmethystRing1', 'RingOfChange', 'RandomRingBox', 'AffinityLvl2', 'AffinityLvl1', 'AffinityArmlet'],
       'FamiliarItems': ['MutationPotion1', 'MutationPotion2', 'MutationPotion3', 'EffigyOfRobustness', 'FamiliarFood', 'BangBear'],
       'MiscItems': ['ExquisiteSilk', 'ImpeccableCashmere', 'SophisticatedSatin', 'LoudSpeaker', 'PocketWatch', 'Prestige2', 'Prestige1', 'BanquetFavor', 'PressCard', 'CourageTablet', 'LookingGlass', 'AdvancedItemDonation2', 'AdvancedItemDonationPart', 'ItemDonation1', 'ItemDonationPart', 'GuildEXPChest4', 'GuildEXPChest3', 'GuildEXPChest2', 'GuildEXPChest1', 'BanishmentStandardFlag', 'ConclaveStandardFlag', 'ConclaveStandardFlagPart', 'SolidarityStandardFlag', 'SanctuaryStandardFlag', 'BanquetFavor2', 'GrandBanquetInvitation', 'GrandBanquetDecor', 'LuxuryBanquetInvitation', 'LuxuryBanquetDecor', 'RematchFlag', 'AllyFlag', 'ChallengeFlag', 'GuardianFlag', 'HuntFlag', 'ArenaMedal', 'AdvancedLeague3', 'InterLeague2', 'NoviceLeague1', 'MirageMirror', 'RenameCard', 'BackgroundWinter', 'BackgroundFireworks'],
@@ -4596,7 +4596,7 @@ export default function GameCalculator() {
   
   // Define consistent attribute ordering
   const attributeOrder = ['strength', 'allure', 'intellect', 'spirit'] as const
-  const bookCategoryOrder = ['Strength', 'Allure', 'Intellect', 'Spirit'] as const
+  const bookCategoryOrder = ['Strength', 'Allure', 'Intellect', 'Spirit', 'Balanced'] as const
 
   // Get attribute colors
   const getAttributeColor = (attribute: string) => {
@@ -5633,6 +5633,14 @@ export default function GameCalculator() {
             <Card className="bg-gray-800/50 border-gray-600">
               <CardHeader>
                 <CardTitle className="text-red-400">Books</CardTitle>
+                <div className="text-lg font-semibold text-yellow-400">
+                  Total Dominance from Books: {Object.entries(books).reduce((total, [category, bookCollection]) => {
+                    return total + Object.entries(bookCollection).reduce((categoryTotal, [bookName, count]) => {
+                      const bonus = bookBonuses[category as keyof typeof bookBonuses]?.[bookName as keyof typeof bookBonuses[typeof category]] || 0
+                      return categoryTotal + (count as number) * bonus
+                    }, 0)
+                  }, 0).toLocaleString()}
+                </div>
               </CardHeader>
               <CardContent>
                 <div className="grid grid-cols-2 gap-4">
